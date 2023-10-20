e3a5e994d99ba837b00ad4b4d591f09f411d95e4
Jason rader <Jasonmrader@outlook.com>
2023-10-19 23:28:26 -0400

    added more tweeks to convexHull method

--- FloorplanClassLibrary/ConvexHull.cs
+++ FloorplanClassLibrary/ConvexHull.cs
@@ -8,45 +8,123 @@ namespace FloorplanClassLibrary
 {
     public class ConvexHull
     {
-        public static List<TablePoint> ComputeConvexHull(List<TablePoint> points)
+
+        public static List<TablePoint> OLDComputeConvexHull(List<TablePoint> points)
         {
-            TablePoint anchor = points.First();
+            if (points.Count < 3)
+                return points; // Can't form a polygon
 
             // Find the bottom-most point
-            foreach (var point in points)
-            {
-                if (point.Y < anchor.Y || (point.Y == anchor.Y && point.X < anchor.X))
-                {
-                    anchor = point;
-                }
-            }
+            TablePoint anchor = points.OrderBy(p => p.Y).ThenBy(p => p.X).First();
 
-            // Sort by polar angle
+            // Sort the rest based on polar angle relative to anchor
             var orderedPoints = points.OrderBy(p => Math.Atan2(p.Y - anchor.Y, p.X - anchor.X)).ToList();
 
-            var hull = new Stack<TablePoint>();
-            hull.Push(orderedPoints[0]);
-            hull.Push(orderedPoints[1]);
+            List<TablePoint> hull = new List<TablePoint>
+            {
+                orderedPoints[0],
+                orderedPoints[1]
+            };
 
             for (int i = 2; i < orderedPoints.Count; i++)
             {
-                TablePoint top = hull.Pop();
-                while (hull.Count > 0 && CrossProduct(orderedPoints[i], top, hull.Peek()) <= 0)
+                while (hull.Count > 1 && CrossProduct(hull[hull.Count - 2], hull[hull.Count - 1], orderedPoints[i]) <= 0)
                 {
-                    top = hull.Pop();
+                    hull.RemoveAt(hull.Count - 1);
                 }
 
-
-                hull.Push(top);
-                hull.Push(orderedPoints[i]);
+                hull.Add(orderedPoints[i]);
             }
 
-            return hull.ToList();
+            return hull;
         }
+        public static List<TablePoint> ComputeConvexHull(List<TablePoint> points)
+        {
+            if (points.Count < 3)
+                return points; // Can't form a polygon
+
+            // Find the bottom-most point
+            TablePoint anchor = points.OrderBy(p => p.Y).ThenBy(p => p.X).First();
+
+            // Sort the rest based on polar angle relative to anchor
+            var orderedPoints = points.OrderBy(p => Math.Atan2(p.Y - anchor.Y, p.X - anchor.X)).ToList();
+
+            List<TablePoint> hull = new List<TablePoint>
+            {
+                orderedPoints[0],
+                orderedPoints[1]
+            };
+
+            int allowedRightTurns = 2; // Number of allowed right turns
+            List<TablePoint> backupHull = null;
+            for (int i = 2; i < orderedPoints.Count; i++)
+            {
+                while (hull.Count > 1 && CrossProduct(hull[hull.Count - 2], hull[hull.Count - 1], orderedPoints[i]) <= 0)
+                {
+                    if (allowedRightTurns > 0)
+                    {
+                        if (backupHull == null)
+                        {
+                            backupHull = new List<TablePoint>(hull); // Backup the current hull state
+                        }
+                        allowedRightTurns--;
+                        break;  // Don't remove the point, just break out and continue
+                    }
+                    else
+                    {
+                        hull.RemoveAt(hull.Count - 1);
+                    }
+                }
+                hull.Add(orderedPoints[i]);
+
+                // If we've made right turns, check if all points are inside the shape
+                if (backupHull != null && allowedRightTurns < 2 && !AllPointsInsideHull(points, hull))
+                {
+                    // Restore the hull to the state before making the right turns and reset allowed right turns
+                    hull = backupHull;
+                    allowedRightTurns = 2;
+                    backupHull = null;
+                }
+            }
+            return hull;
+        
+        }
+
 
         private static double CrossProduct(TablePoint a, TablePoint b, TablePoint c)
         {
             return (b.X - a.X) * (c.Y - a.Y) - (b.Y - a.Y) * (c.X - a.X);
         }
+        private static bool AllPointsInsideHull(List<TablePoint> points, List<TablePoint> hull)
+        {
+            foreach (var point in points)
+            {
+                if (!IsPointInsidePolygon(point, hull))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
+        private static bool IsPointInsidePolygon(TablePoint p, List<TablePoint> polygon)
+        {
+            bool isInside = false;
+
+            // Loop through each pair of vertices
+            int j = polygon.Count - 1;
+            for (int i = 0; i < polygon.Count; i++)
+            {
+                // If the point lies on an edge, it's considered inside
+                if ((polygon[i].Y > p.Y) != (polygon[j].Y > p.Y) &&
+                    p.X < (polygon[j].X - polygon[i].X) * (p.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) + polygon[i].X)
+                {
+                    isInside = !isInside;
+                }
+                j = i;
+            }
+
+            return isInside;
+        }
+
     }
+}
