4346cacf58944b9209e4e7ffdb3e49790430fa69
Jason rader <Jasonmrader@outlook.com>
2023-12-10 20:56:43 -0500

    removed section LIne from test

--- FloorPlanMaker/Form1.cs
+++ FloorPlanMaker/Form1.cs
@@ -884,31 +884,31 @@ namespace FloorPlanMaker
 
             Bitmap edgesBitmap = edgeDrawer.CreateEdgeBitmap(pnlFloorPlan.Size, boarderManager.Sections.SelectMany(s => s.SectionBoarders.Edges));
             pnlFloorPlan.BackgroundImage = edgesBitmap;
-            foreach (Section section in boarderManager.Sections)
-            {
-                //section.SetBoarderManager();
+            //foreach (Section section in boarderManager.Sections)
+            //{
+            //    //section.SetBoarderManager();
 
-                //List<Edge> hullEdges = nodeManager.getEdgesForConvexHull();
-                foreach (Edge edge in section.SectionBoarders.Edges)
-                {
-                    //NodeControl nodeControl1 = new NodeControl(edge.StartNode, NodeControl.NodePosition.Top);
-                    SectionLine line = new SectionLine(edge);
-                    line.LineThickness = 20f;
-                    line.Invalidate();
-                    if(edge.isVertical)
-                    {
-                        line.Width = 10;
-                    }
-                    else
-                    {
-                        line.Height = 10;
-                    }
-                    line.LineColor = section.Color;
-                    line.BringToFront();
-                    pnlFloorPlan.Controls.Add(line);
-                    //pnlFloorPlan.Controls.Add((NodeControl)nodeControl1);
-                }
-            }
+            //    //List<Edge> hullEdges = nodeManager.getEdgesForConvexHull();
+            //    foreach (Edge edge in section.SectionBoarders.Edges)
+            //    {
+            //        //NodeControl nodeControl1 = new NodeControl(edge.StartNode, NodeControl.NodePosition.Top);
+            //        SectionLine line = new SectionLine(edge);
+            //        line.LineThickness = 20f;
+            //        line.Invalidate();
+            //        if(edge.isVertical)
+            //        {
+            //            line.Width = 10;
+            //        }
+            //        else
+            //        {
+            //            line.Height = 10;
+            //        }
+            //        line.LineColor = section.Color;
+            //        line.BringToFront();
+            //        pnlFloorPlan.Controls.Add(line);
+            //        //pnlFloorPlan.Controls.Add((NodeControl)nodeControl1);
+            //    }
+            //}
             //boarderManager.CalculateOverlappingSectionEdges();
             //foreach (Edge edge in boarderManager.OverLappingEdges)
             //{
