11d164cf94593d6113b6121ea65f3805e4c728a2
Jason rader <Jasonmrader@outlook.com>
2023-12-18 22:45:51 -0500

    Changes to neighbor class

--- FloorplanClassLibrary/Neighbors.cs
+++ FloorplanClassLibrary/Neighbors.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace FloorplanClassLibrary
 {
-    internal class Neighbors
+    public class Neighbors
     {
+        public Neighbors() { }
+        public TableEdgeBorders? RightNeighbor { get; set; }
+        public TableEdgeBorders? LeftNeighbor { get; set; }
+        public TableEdgeBorders? TopNeighbor { get; set; }
+        public TableEdgeBorders? BottomNeighbor { get; set; }
+        public int MidPoint { get; set; }
+        public Edge Edge { get; set; } 
+        
+        
     }
 }
