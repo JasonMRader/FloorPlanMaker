7802d0d768b47b370f90b98246044be46bcdc717
Jason rader <Jasonmrader@outlook.com>
2023-12-20 15:28:59 -0500

    Created OverriddenTablePairs class

--- FloorplanClassLibrary/OverriddenTablePairs.cs
+++ FloorplanClassLibrary/OverriddenTablePairs.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace FloorplanClassLibrary
 {
-    internal class OverriddenTablePairs
+    public class OverriddenTablePairs
     {
+        public OverriddenTablePairs() { }
+        public Dictionary<string, string> hardcodedTopBottomNeighbors = new Dictionary<string, string>()
+        {
+            {"42", "41"},
+            {"52", "61"},
+            {"61", "52"},
+            {"441","300" }
+
+        };
+        public Dictionary<string, string> hardcodedRightLeftNeighbors = new Dictionary<string, string>()
+        {
+
+
+        };
+
+        public HashSet<string> ignorePairs = new HashSet<string>()
+        {
+            "61-53",
+            "51-63",
+            "61-63",
+            "51-53",
+            "445-300",
+            "65-54",
+            "434-445",
+            "441-418",
+            "418-441"
+
+
+
+        };
+        public string GetPairKey(string tableNumberOne, string tableNumberTwo)
+        {
+            return tableNumberOne.CompareTo(tableNumberTwo) < 0
+                ? tableNumberOne + "-" + tableNumberTwo
+                : tableNumberTwo + "-" + tableNumberOne;
+        }
     }
 }
