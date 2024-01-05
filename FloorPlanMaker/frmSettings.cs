db7166878d16474d9c13df0b5747626cd3157f62
Jason rader <Jasonmrader@outlook.com>
2024-01-04 21:26:15 -0500

    changes to database, added settings to published app

--- FloorPlanMaker/frmSettings.cs
+++ FloorPlanMaker/frmSettings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FloorplanClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -29,7 +30,7 @@ namespace FloorPlanMakerUI
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 string filePath = openFileDialog.FileName;
-                LoadDatabaseTables(filePath);
+                SqliteDataAccess.LoadDatabaseTables(filePath);
             }
         }
     }
