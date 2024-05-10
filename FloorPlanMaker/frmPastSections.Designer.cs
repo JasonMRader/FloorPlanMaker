9cb67a1b1df8bc3ab24ff6b7f366e2f9983ffa7e
Jason rader <Jasonmrader@outlook.com>
2024-05-10 15:14:02 -0400

    Update

--- FloorPlanMaker/frmPastSections.Designer.cs
+++ FloorPlanMaker/frmPastSections.Designer.cs
@@ -99,7 +99,7 @@
             listBox1.ItemHeight = 15;
             listBox1.Location = new Point(40, 85);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(450, 349);
+            listBox1.Size = new Size(856, 349);
             listBox1.TabIndex = 6;
             // 
             // frmPastSections
