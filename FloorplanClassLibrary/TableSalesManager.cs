cd772c1fa3b7a92984b5a41a375ced39382a0439
Jason rader <Jasonmrader@outlook.com>
2024-05-14 14:25:38 -0400

    began reformatting table stats

--- FloorplanClassLibrary/TableSalesManager.cs
+++ FloorplanClassLibrary/TableSalesManager.cs
@@ -12,7 +12,10 @@ namespace FloorplanClassLibrary
     {
         public TableSalesManager() { }
         public List<TableStat> Stats { get; set; } = new List<TableStat>();
-        public float? FloorplanSalesTotal
+        public List<DateOnly> DatesAveraged { get; set; }
+        public bool IsAm { get; set; }
+        
+        public float? DiningAreaExpectedSales
         {
             get
             {
@@ -45,33 +48,6 @@ namespace FloorplanClassLibrary
 
             return groupedOrders;
         }
-        public List<TableStat> GetStatsByShiftAndDayOfWeek(List<TableStat> allStats, bool isLunch, DayOfWeek dayOfWeek)
-        {
-            return allStats
-                .Where(ts => ts.IsLunch == isLunch && ts.DayOfWeek == dayOfWeek)
-                .ToList();
-        }
-        public List<TableStat> GetStatsByDateRange(List<TableStat> allStats, DateOnly startDate, DateOnly endDate)
-        {
-            return allStats
-                .Where(ts => ts.Date >= startDate && ts.Date <= endDate)
-                .ToList();
-        }
-        public void SetTableStats(List<Table> tables, bool isLunch, DateOnly dateOnly)
-        {
-            List<TableStat> tableStats = SqliteDataAccess.LoadTableStats();
-            TableSalesManager tableSalesManager = new TableSalesManager();
-            List<TableStat> filteredTableStats = tableSalesManager.GetStatsByShiftAndDayOfWeek(tableStats, isLunch, dateOnly.DayOfWeek);
-            foreach (Table table in tables)
-            {
-                var matchedStat = filteredTableStats.FirstOrDefault(t => t.TableStatNumber == table.TableNumber);
-                if (matchedStat != null)
-                {
-                    table.AverageSales = (float)matchedStat.Sales;
-                }
-                else { table.AverageSales = -1; }
-            }
-        }
         public List<OrderDetail> ReadOrderDetails(string filePath)
         {
             using (var reader = new StreamReader(filePath))
@@ -81,20 +57,46 @@ namespace FloorplanClassLibrary
                 return records;
             }
         }
-        public void AnalyzeSales(List<OrderDetail> orders)
+        public void SetDateToToday(DateOnly dateOnly)
         {
-            var before4pm = orders.Where(x => x.Opened.Hour < 16);
-            var after4pm = orders.Where(x => x.Opened.Hour >= 16);
 
-            var avgSalesBefore4pm = before4pm
-                .GroupBy(x => x.Table)
-                .Select(g => new { Table = g.Key, AverageSales = g.Average(x => x.Amount) });
-
-            var avgSalesAfter4pm = after4pm
-                .GroupBy(x => x.Table)
-                .Select(g => new { Table = g.Key, AverageSales = g.Average(x => x.Amount) });
-
-            // Output or further processing of avgSalesBefore4pm and avgSalesAfter4pm
         }
+        public void SetDateToYesterday(DateOnly dateOnly)
+        {
+
+        }
+        public void SetDateToLastWeek(DateOnly dateOnly)
+        {
+
+        }
+        //public List<TableStat> GetStatsByShiftAndDayOfWeek(List<TableStat> allStats, bool isLunch, DayOfWeek dayOfWeek)
+        //{
+        //    return allStats
+        //        .Where(ts => ts.IsLunch == isLunch && ts.DayOfWeek == dayOfWeek)
+        //        .ToList();
+        //}
+        //public List<TableStat> GetStatsByDateRange(List<TableStat> allStats, DateOnly startDate, DateOnly endDate)
+        //{
+        //    return allStats
+        //        .Where(ts => ts.Date >= startDate && ts.Date <= endDate)
+        //        .ToList();
+        //}
+        //public void SetTableStats(List<Table> tables, bool isLunch, DateOnly dateOnly)
+        //{
+        //    List<TableStat> tableStats = SqliteDataAccess.LoadTableStats();
+        //    TableSalesManager tableSalesManager = new TableSalesManager();
+        //    List<TableStat> filteredTableStats = tableSalesManager.GetStatsByShiftAndDayOfWeek(tableStats, isLunch, dateOnly.DayOfWeek);
+        //    foreach (Table table in tables)
+        //    {
+        //        var matchedStat = filteredTableStats.FirstOrDefault(t => t.TableStatNumber == table.TableNumber);
+        //        if (matchedStat != null)
+        //        {
+        //            table.AverageSales = (float)matchedStat.Sales;
+        //        }
+        //        else { table.AverageSales = -1; }
+        //    }
+        //}
+
+
     }
 }
