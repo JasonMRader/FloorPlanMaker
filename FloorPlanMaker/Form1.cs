4847edda7f3a8d0857e2db583a0225bffa3018fd
Jason rader <Jasonmrader@outlook.com>
2024-05-18 14:34:36 -0400

    disabled printing

--- FloorPlanMaker/Form1.cs
+++ FloorPlanMaker/Form1.cs
@@ -632,7 +632,7 @@ namespace FloorPlanMaker
                 if (printWihtLines == DialogResult.No)
                 {
                     FloorplanPrinter printerNoLines = new FloorplanPrinter(pnlFloorPlan);
-                    printerNoLines.ShowPrintPreview();
+                    //printerNoLines.ShowPrintPreview();
                     return;
                 }
                 
@@ -650,7 +650,7 @@ namespace FloorPlanMaker
                 //Bitmap edgesBitmap = edgeDrawer.CreateEdgeBitmap(pnlFloorPlan.Size, edges);
                 pnlFloorPlan.BackgroundImage = edgesBitmap;
                 FloorplanPrinter printer = new FloorplanPrinter(pnlFloorPlan, edgeDrawer, grid.GetSectionTableBoarders());
-                printer.ShowPrintPreview();
+               // printer.ShowPrintPreview();
                 //printer.Print();
             }
             else
