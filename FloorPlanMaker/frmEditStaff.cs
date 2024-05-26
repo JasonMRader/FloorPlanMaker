61da841bca63e63b986afad0ff598a916266c8c1
Jason rader <Jasonmrader@outlook.com>
2024-05-26 14:35:56 -0400

    Revert "Added ShiftManager to editStaff Form"
    This reverts commit d4ebe65bb6ce91122b0680e08f19527c221c5001.

--- FloorPlanMaker/frmEditStaff.cs
+++ FloorPlanMaker/frmEditStaff.cs
@@ -15,7 +15,6 @@ namespace FloorPlanMaker
         public EmployeeManager employeeManager;
         private Shift newShiftManager = new Shift();
         private Shift pastShiftsManager;
-        private ShiftManager ShiftManager = new ShiftManager();
         private DiningAreaCreationManager DiningAreaManager = new DiningAreaCreationManager();
         private DateTime dateSelected = DateTime.MinValue;
         private List<Floorplan> allFloorplans = new List<Floorplan>();
