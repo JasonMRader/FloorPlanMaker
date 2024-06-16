17c47db91b53a3c2a4362e666d5ceb2bf24a7699
Jason rader <Jasonmrader@outlook.com>
2024-06-16 12:59:10 -0400

    Update

--- FloorPlanMaker/frmSalesStats.Designer.cs
+++ FloorPlanMaker/frmSalesStats.Designer.cs
@@ -57,13 +57,11 @@
             lblComboLabel = new Label();
             btnIndividualStats = new Button();
             cboServerSelect = new ComboBox();
-            numericUpDown1 = new NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)dgvDiningAreas).BeginInit();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
             panel3.SuspendLayout();
             panel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // dgvDiningAreas
@@ -321,7 +319,6 @@
             // 
             // panel4
             // 
-            panel4.Controls.Add(numericUpDown1);
             panel4.Controls.Add(btnIndividualServerShifts);
             panel4.Controls.Add(lblComboLabel);
             panel4.Controls.Add(btnIndividualStats);
@@ -369,13 +366,6 @@
             cboServerSelect.Size = new Size(259, 23);
             cboServerSelect.TabIndex = 0;
             // 
-            // numericUpDown1
-            // 
-            numericUpDown1.Location = new Point(52, 123);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(120, 23);
-            numericUpDown1.TabIndex = 4;
-            // 
             // frmSalesStats
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -403,7 +393,6 @@
             panel3.ResumeLayout(false);
             panel4.ResumeLayout(false);
             panel4.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -438,6 +427,5 @@
         private Label lblComboLabel;
         private Button btnIndividualStats;
         private Button btnIndividualServerShifts;
-        private NumericUpDown numericUpDown1;
     }
 }
