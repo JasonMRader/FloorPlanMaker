dee599c1fb03e57ae0007e7a824069898f7481e0
Jason rader <Jasonmrader@outlook.com>
2024-06-16 15:30:32 -0400

    Created control for servers to more them to and from floorplans

---
+++ FloorplanUserControlLibrary/ServerInFloorplanControl.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FloorplanUserControlLibrary
+{
+    partial class ServerInFloorplanControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(100, 130, 180);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Semibold", 12.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(186, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Adrianna";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FlatStyle = FlatStyle.Flat;
+            comboBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(186, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(114, 29);
+            comboBox1.TabIndex = 1;
+            // 
+            // ServerInFloorplanControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Name = "ServerInFloorplanControl";
+            Size = new Size(300, 80);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private ComboBox comboBox1;
+    }
+}
