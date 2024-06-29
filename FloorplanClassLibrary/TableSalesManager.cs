875bd260a560345342527c093e519fa22c4ab268
Jason rader <Jasonmrader@outlook.com>
2024-06-29 12:14:24 -0400

    cleaned code in tableSalesManager

--- FloorplanClassLibrary/TableSalesManager.cs
+++ FloorplanClassLibrary/TableSalesManager.cs
@@ -32,50 +32,16 @@ namespace FloorplanClassLibrary
                 return Stats.Sum(stat => stat.Sales);
             }
         }
-        public float DiningAreaExpectedSales(DiningArea diningArea)
-        {
-            string test = "";
-            float totalAreaSales = 0f;
-
-            var insideBarSales = this.Stats
-                .Where(t => t.TableStatNumber.CompareTo("120") >= 0 && t.TableStatNumber.CompareTo("155") <= 0)
-                .Sum(t => t.Sales);
-            foreach (Table table in diningArea.Tables)
-            {
-                if (table.TableNumber == "INSIDE BAR")
-                {
-                    table.AverageSales = (float)insideBarSales;
-                    totalAreaSales += (float)insideBarSales;
-                    test += $"\n{table.TableNumber} : {insideBarSales} : {totalAreaSales}";
-                }
-                else
-                {
-                    var matchedStat =this.Stats.FirstOrDefault(t => t.TableStatNumber == table.TableNumber);
-                    if (matchedStat != null)
-                    {
-                        table.AverageSales = (float)matchedStat.Sales;
-                        totalAreaSales += (float)matchedStat.Sales;
-                        test += $"\n{table.TableNumber} : {matchedStat.Sales} : {totalAreaSales}";
-                    }
-                    else
-                    {
-                        table.AverageSales = 0;
-                    }
-                }
-            }
-            DiningAreaTotalSalesDisplay = Section.FormatAsCurrencyWithoutParentheses(totalAreaSales);
-            return totalAreaSales;
-            
-        }
+       
         public List<TableStat> ProcessCsvFile(string filePath)
         {
             var orders = ReadOrderDetails(filePath);
 
             var groupedOrders = orders
-                .Where(order => order.Table != "")  // Exclude orders without a table number
+                .Where(order => order.Table != "") 
                 .GroupBy(order => new
                 {
-                    Table = order.Table,  // Safe to use .Value here due to the filter above
+                    Table = order.Table,  
                     Date = DateOnly.FromDateTime(order.Opened),
                     IsLunch = order.Opened.Hour < 16
                 })
@@ -148,55 +114,7 @@ namespace FloorplanClassLibrary
 
 
             this.Stats = SqliteDataAccess.LoadTableStatsByDateListAndLunch(isAM, previousWeekdays);
-        }
-        private static List<TableStat> CalculateAverageSales(List<TableStat> tableStats, int numberOfDays)
-        {
-            // Group the table stats by TableStatNumber
-            var groupedStats = tableStats
-                .GroupBy(ts => ts.TableStatNumber)
-                .Select(group => new
-                {
-                    TableStatNumber = group.Key,
-                    TotalSales = group.Sum(g => g.Sales ?? 0)
-                });
-
-            // Calculate average sales and create new TableStat objects
-            var averageStats = groupedStats
-                .Select(g => new TableStat
-                {
-                    TableStatNumber = g.TableStatNumber,
-                    Sales = g.TotalSales / numberOfDays
-                }).ToList();
-
-            return averageStats;
-        }
-        //public List<TableStat> GetStatsByShiftAndDayOfWeek(List<TableStat> allStats, bool isLunch, DayOfWeek dayOfWeek)
-        //{
-        //    return allStats
-        //        .Where(ts => ts.IsLunch == isLunch && ts.DayOfWeek == dayOfWeek)
-        //        .ToList();
-        //}
-        //public List<TableStat> GetStatsByDateRange(List<TableStat> allStats, DateOnly startDate, DateOnly endDate)
-        //{
-        //    return allStats
-        //        .Where(ts => ts.Date >= startDate && ts.Date <= endDate)
-        //        .ToList();
-        //}
-        //public void SetTableStats(List<Table> tables, bool isLunch, DateOnly dateOnly)
-        //{
-        //    List<TableStat> tableStats = SqliteDataAccess.LoadTableStats();
-        //    TableSalesManager tableSalesManager = new TableSalesManager();
-        //    List<TableStat> filteredTableStats = tableSalesManager.GetStatsByShiftAndDayOfWeek(tableStats, isLunch, dateOnly.DayOfWeek);
-        //    foreach (Table table in tables)
-        //    {
-        //        var matchedStat = filteredTableStats.FirstOrDefault(t => t.TableStatNumber == table.TableNumber);
-        //        if (matchedStat != null)
-        //        {
-        //            table.AverageSales = (float)matchedStat.Sales;
-        //        }
-        //        else { table.AverageSales = -1; }
-        //    }
-        //}
+        }      
 
 
     }
