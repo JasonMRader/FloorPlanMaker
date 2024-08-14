f6337ec204711149bdc3ad120430bd8c5db7e4a8
Jason rader <Jasonmrader@outlook.com>
2024-08-14 18:03:21 -0400

    ud

---
+++ FloorPlanMaker/ClickOutsideDialogFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloorPlanMakerUI
+{
+    public class ClickOutsideDialogFilter : IMessageFilter
+    {
+        private readonly Form _form;
+
+        public ClickOutsideDialogFilter(Form form)
+        {
+            _form = form;
+        }
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == 0x201) // WM_LBUTTONDOWN message
+            {
+                Point cursorPosition = Control.MousePosition;
+                if (!_form.Bounds.Contains(cursorPosition))
+                {
+                    _form.DialogResult = DialogResult.Cancel;
+                    _form.Close();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+}
