083593ad70403529b7bf7ef865ad9d17caa5c690
Jason rader <Jasonmrader@outlook.com>
2024-08-15 14:46:12 -0400

    PanelManager Changes

--- FloorPlanMaker/SectionPanelManager.cs
+++ FloorPlanMaker/SectionPanelManager.cs
@@ -10,27 +10,40 @@ namespace FloorPlanMakerUI
 {
     public class SectionPanelManager : IFloorplanObserver
     {
-        private List<SectionPanelControl> _sectionPanels = new List<SectionPanelControl>();
-        public List<SectionPanelControl> SectionPanels { get {  return _sectionPanels; } }
+        private List<SectionInfoPanel> _sectionPanels = new List<SectionInfoPanel>();
+        public List<SectionInfoPanel> SectionPanels { get {  return _sectionPanels; } }
         private Floorplan _floorplan { get; set; }
         public Floorplan Floorplan { get {  return _floorplan; } }
         private FlowLayoutPanel _flowLayoutPanel = new FlowLayoutPanel();
         public SectionPanelManager(Floorplan floorplan, FlowLayoutPanel flowLayoutPanel)
         {
             _floorplan = floorplan;
-            
+            floorplan.SubscribeObserver(this);
+            floorplan.SectionRemoved += RemoveSection;
+            floorplan.SectionAdded += AddSection;
             _flowLayoutPanel = flowLayoutPanel;
             if(floorplan != null)
             {
                 AddSectionPanels();
             }
         }
+
+        private void AddSection(Section sectionAdded, Floorplan floorplan)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RemoveSection(Section sectionRemoved, Floorplan floorplan)
+        {
+            throw new NotImplementedException();
+        }
+
         public void ChangeFloorplan(Floorplan floorplan)
         {
 
         }
 
-        public void UpdateFloorplan(Section section)
+        public void UpdateFloorplan(Floorplan floorplan)
         {
             throw new NotImplementedException();
         }
