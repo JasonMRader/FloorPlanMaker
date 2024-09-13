b1c9e239aad684d2e87dbecd432e00539aa0be65
Jason rader <Jasonmrader@outlook.com>
2024-09-13 13:16:05 -0400

    Added txtBoxes to Color Selection control, more UI improvements

--- FloorplanUserControlLibrary/ColorSelection.Designer.cs
+++ FloorplanUserControlLibrary/ColorSelection.Designer.cs
@@ -35,46 +35,59 @@
             lblDefaults = new Label();
             lblSectionNumber = new Label();
             panel7 = new Panel();
+            txtB = new TextBox();
             panel8 = new Panel();
+            txtG = new TextBox();
             panel9 = new Panel();
+            txtR = new TextBox();
             lbl2Before = new Label();
             lbl3Before = new Label();
             lbl1Before = new Label();
             lbl2After = new Label();
             lbl3After = new Label();
             lbl1After = new Label();
+            pnlMain = new Panel();
+            panel2 = new Panel();
             ((System.ComponentModel.ISupportInitialize)tbB).BeginInit();
             ((System.ComponentModel.ISupportInitialize)tbG).BeginInit();
             ((System.ComponentModel.ISupportInitialize)tbR).BeginInit();
+            panel7.SuspendLayout();
+            panel8.SuspendLayout();
+            panel9.SuspendLayout();
+            pnlMain.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // tbB
             // 
+            tbB.BackColor = Color.WhiteSmoke;
             tbB.LargeChange = 25;
-            tbB.Location = new Point(30, 298);
+            tbB.Location = new Point(73, 4);
             tbB.Maximum = 255;
             tbB.Name = "tbB";
-            tbB.Size = new Size(952, 45);
+            tbB.Size = new Size(892, 45);
             tbB.TabIndex = 0;
             tbB.Scroll += tbB_Scroll;
             // 
             // tbG
             // 
+            tbG.BackColor = Color.WhiteSmoke;
             tbG.LargeChange = 25;
-            tbG.Location = new Point(30, 247);
+            tbG.Location = new Point(73, 5);
             tbG.Maximum = 255;
             tbG.Name = "tbG";
-            tbG.Size = new Size(952, 45);
+            tbG.Size = new Size(892, 45);
             tbG.TabIndex = 0;
             tbG.Scroll += tbG_Scroll;
             // 
             // tbR
             // 
+            tbR.BackColor = Color.WhiteSmoke;
             tbR.LargeChange = 25;
-            tbR.Location = new Point(30, 196);
+            tbR.Location = new Point(73, 4);
             tbR.Maximum = 255;
             tbR.Name = "tbR";
-            tbR.Size = new Size(952, 45);
+            tbR.Size = new Size(892, 45);
             tbR.TabIndex = 0;
             tbR.Scroll += tbR_Scroll;
             // 
@@ -85,9 +98,9 @@
             cbForeColor.FlatAppearance.BorderSize = 0;
             cbForeColor.FlatStyle = FlatStyle.Flat;
             cbForeColor.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            cbForeColor.Location = new Point(8, 136);
+            cbForeColor.Location = new Point(10, 130);
             cbForeColor.Name = "cbForeColor";
-            cbForeColor.Size = new Size(974, 36);
+            cbForeColor.Size = new Size(980, 36);
             cbForeColor.TabIndex = 2;
             cbForeColor.Text = "Text Color White";
             cbForeColor.TextAlign = ContentAlignment.MiddleCenter;
@@ -96,24 +109,26 @@
             // 
             // flowDefaults
             // 
-            flowDefaults.Location = new Point(8, 387);
+            flowDefaults.Location = new Point(10, 390);
             flowDefaults.Name = "flowDefaults";
-            flowDefaults.Size = new Size(974, 72);
+            flowDefaults.Size = new Size(980, 72);
             flowDefaults.TabIndex = 3;
             // 
             // lblDefaults
             // 
-            lblDefaults.AutoSize = true;
-            lblDefaults.Location = new Point(8, 362);
+            lblDefaults.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblDefaults.Location = new Point(9, 355);
             lblDefaults.Name = "lblDefaults";
-            lblDefaults.Size = new Size(50, 15);
+            lblDefaults.Size = new Size(980, 32);
             lblDefaults.TabIndex = 4;
             lblDefaults.Text = "Defaults";
+            lblDefaults.TextAlign = ContentAlignment.MiddleCenter;
+            lblDefaults.Click += lblDefaults_Click;
             // 
             // lblSectionNumber
             // 
             lblSectionNumber.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            lblSectionNumber.Location = new Point(385, 39);
+            lblSectionNumber.Location = new Point(384, 40);
             lblSectionNumber.Name = "lblSectionNumber";
             lblSectionNumber.Size = new Size(240, 23);
             lblSectionNumber.TabIndex = 5;
@@ -123,32 +138,66 @@
             // panel7
             // 
             panel7.BackColor = Color.Blue;
-            panel7.Location = new Point(8, 299);
+            panel7.Controls.Add(txtB);
+            panel7.Controls.Add(tbB);
+            panel7.Location = new Point(4, 124);
             panel7.Name = "panel7";
-            panel7.Size = new Size(18, 29);
+            panel7.Size = new Size(972, 53);
             panel7.TabIndex = 6;
+            // 
+            // txtB
+            // 
+            txtB.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtB.Location = new Point(4, 4);
+            txtB.Name = "txtB";
+            txtB.Size = new Size(64, 46);
+            txtB.TabIndex = 1;
+            txtB.TextChanged += txtB_TextChanged;
+            txtB.Leave += txtG_TextChanged;
             // 
             // panel8
             // 
             panel8.BackColor = Color.Green;
-            panel8.Location = new Point(8, 247);
+            panel8.Controls.Add(txtG);
+            panel8.Controls.Add(tbG);
+            panel8.Location = new Point(4, 64);
             panel8.Name = "panel8";
-            panel8.Size = new Size(18, 29);
+            panel8.Size = new Size(972, 53);
             panel8.TabIndex = 6;
+            // 
+            // txtG
+            // 
+            txtG.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtG.Location = new Point(4, 4);
+            txtG.Name = "txtG";
+            txtG.Size = new Size(64, 46);
+            txtG.TabIndex = 1;
+            txtG.Leave += txtG_TextChanged;
             // 
             // panel9
             // 
             panel9.BackColor = Color.Red;
-            panel9.Location = new Point(8, 196);
+            panel9.Controls.Add(txtR);
+            panel9.Controls.Add(tbR);
+            panel9.Location = new Point(4, 4);
             panel9.Name = "panel9";
-            panel9.Size = new Size(18, 29);
+            panel9.Size = new Size(972, 53);
             panel9.TabIndex = 6;
+            // 
+            // txtR
+            // 
+            txtR.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtR.Location = new Point(4, 4);
+            txtR.Name = "txtR";
+            txtR.Size = new Size(64, 46);
+            txtR.TabIndex = 1;
+            txtR.Leave += txtR_TextChanged;
             // 
             // lbl2Before
             // 
             lbl2Before.BackColor = Color.FromArgb(224, 224, 224);
             lbl2Before.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl2Before.Location = new Point(132, 21);
+            lbl2Before.Location = new Point(131, 22);
             lbl2Before.Margin = new Padding(2);
             lbl2Before.Name = "lbl2Before";
             lbl2Before.Size = new Size(120, 60);
@@ -160,7 +209,7 @@
             // 
             lbl3Before.BackColor = Color.FromArgb(224, 224, 224);
             lbl3Before.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl3Before.Location = new Point(8, 21);
+            lbl3Before.Location = new Point(7, 22);
             lbl3Before.Margin = new Padding(2);
             lbl3Before.Name = "lbl3Before";
             lbl3Before.Size = new Size(120, 60);
@@ -172,7 +221,7 @@
             // 
             lbl1Before.BackColor = Color.FromArgb(224, 224, 224);
             lbl1Before.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl1Before.Location = new Point(256, 21);
+            lbl1Before.Location = new Point(255, 22);
             lbl1Before.Margin = new Padding(2);
             lbl1Before.Name = "lbl1Before";
             lbl1Before.Size = new Size(120, 60);
@@ -184,7 +233,7 @@
             // 
             lbl2After.BackColor = Color.FromArgb(224, 224, 224);
             lbl2After.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl2After.Location = new Point(748, 21);
+            lbl2After.Location = new Point(747, 22);
             lbl2After.Margin = new Padding(2);
             lbl2After.Name = "lbl2After";
             lbl2After.Size = new Size(120, 60);
@@ -196,7 +245,7 @@
             // 
             lbl3After.BackColor = Color.FromArgb(224, 224, 224);
             lbl3After.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl3After.Location = new Point(872, 21);
+            lbl3After.Location = new Point(871, 22);
             lbl3After.Margin = new Padding(2);
             lbl3After.Name = "lbl3After";
             lbl3After.Size = new Size(120, 60);
@@ -208,7 +257,7 @@
             // 
             lbl1After.BackColor = Color.FromArgb(224, 224, 224);
             lbl1After.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl1After.Location = new Point(624, 21);
+            lbl1After.Location = new Point(623, 22);
             lbl1After.Margin = new Padding(2);
             lbl1After.Name = "lbl1After";
             lbl1After.Size = new Size(120, 60);
@@ -216,35 +265,57 @@
             lbl1After.Text = "1";
             lbl1After.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // pnlMain
+            // 
+            pnlMain.BackColor = Color.WhiteSmoke;
+            pnlMain.Controls.Add(panel2);
+            pnlMain.Controls.Add(lbl1After);
+            pnlMain.Controls.Add(flowDefaults);
+            pnlMain.Controls.Add(lbl3Before);
+            pnlMain.Controls.Add(lblDefaults);
+            pnlMain.Controls.Add(lbl3After);
+            pnlMain.Controls.Add(cbForeColor);
+            pnlMain.Controls.Add(lbl1Before);
+            pnlMain.Controls.Add(lbl2After);
+            pnlMain.Controls.Add(lblSectionNumber);
+            pnlMain.Controls.Add(lbl2Before);
+            pnlMain.Location = new Point(10, 10);
+            pnlMain.Name = "pnlMain";
+            pnlMain.Size = new Size(1000, 475);
+            pnlMain.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(panel7);
+            panel2.Controls.Add(panel8);
+            panel2.Controls.Add(panel9);
+            panel2.Location = new Point(9, 175);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(980, 182);
+            panel2.TabIndex = 7;
+            // 
             // ColorSelection
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(lbl1After);
-            Controls.Add(lbl3Before);
-            Controls.Add(lbl3After);
-            Controls.Add(lbl1Before);
-            Controls.Add(lbl2After);
-            Controls.Add(lbl2Before);
-            Controls.Add(panel9);
-            Controls.Add(panel8);
-            Controls.Add(panel7);
-            Controls.Add(lblSectionNumber);
-            Controls.Add(lblDefaults);
-            Controls.Add(flowDefaults);
-            Controls.Add(cbForeColor);
-            Controls.Add(tbR);
-            Controls.Add(tbG);
-            Controls.Add(tbB);
+            BackColor = Color.FromArgb(180, 190, 200);
+            Controls.Add(pnlMain);
             Margin = new Padding(0);
             Name = "ColorSelection";
-            Size = new Size(1000, 475);
+            Size = new Size(1020, 495);
             Load += ColorSelection_Load;
             ((System.ComponentModel.ISupportInitialize)tbB).EndInit();
             ((System.ComponentModel.ISupportInitialize)tbG).EndInit();
             ((System.ComponentModel.ISupportInitialize)tbR).EndInit();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel8.ResumeLayout(false);
+            panel8.PerformLayout();
+            panel9.ResumeLayout(false);
+            panel9.PerformLayout();
+            pnlMain.ResumeLayout(false);
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -265,5 +336,10 @@
         private Label lbl2After;
         private Label lbl3After;
         private Label lbl1After;
+        private Panel pnlMain;
+        private Panel panel2;
+        private TextBox txtB;
+        private TextBox txtG;
+        private TextBox txtR;
     }
 }
--- FloorplanUserControlLibrary/ColorSelection.cs
+++ FloorplanUserControlLibrary/ColorSelection.cs
@@ -28,7 +28,7 @@ namespace FloorplanUserControlLibrary
         {
 
             for (int i = 1; i <= 15; i++) {
-              
+
 
                 Button lbl = new Button() {
                     Text = i.ToString(),
@@ -42,15 +42,15 @@ namespace FloorplanUserControlLibrary
                     AllowDrop = true
                 };
 
-               
-                
+
+
                 lbl.Click += Label_Click;
                 //this.Controls.Add(lbl1Before);
                 //this.Controls.Add(lbl1After);
                 flowDefaults.Controls.Add(lbl);
 
             }
-           
+
 
 
         }
@@ -95,7 +95,7 @@ namespace FloorplanUserControlLibrary
         {
             num = sectionNumber;
             this.colorPair = SectionColorManager.GetColorPair(sectionNumber);
-            this.BackColor = colorPair.BackgroundColor;
+            pnlMain.BackColor = colorPair.BackgroundColor;
             tbR.Value = colorPair.BackgroundColor.R;
             R = colorPair.BackgroundColor.R;
             tbG.Value = colorPair.BackgroundColor.G;
@@ -116,19 +116,22 @@ namespace FloorplanUserControlLibrary
         private void tbR_Scroll(object sender, EventArgs e)
         {
             R = tbR.Value;
-            this.BackColor = Color.FromArgb(R, G, B);
+            pnlMain.BackColor = Color.FromArgb(R, G, B);
+            txtR.Text = R.ToString();
         }
 
         private void tbB_Scroll(object sender, EventArgs e)
         {
             B = tbB.Value;
-            this.BackColor = Color.FromArgb(R, G, B);
+            pnlMain.BackColor = Color.FromArgb(R, G, B);
+            txtB.Text = B.ToString();
         }
 
         private void tbG_Scroll(object sender, EventArgs e)
         {
             G = tbG.Value;
-            this.BackColor = Color.FromArgb(R, G, B);
+            pnlMain.BackColor = Color.FromArgb(R, G, B);
+            txtG.Text = G.ToString();
         }
 
         private void cbForeColor_CheckedChanged(object sender, EventArgs e)
@@ -162,9 +165,52 @@ namespace FloorplanUserControlLibrary
 
         }
 
-        private void panel6_Paint(object sender, PaintEventArgs e)
+
+        private void lblDefaults_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void txtR_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtR.Text)) {
+                if (int.TryParse(txtR.Text, out int r)) {
+                    if (r <= 255 && r >= 0) {
+                        R = r;
+                        tbR.Value = r;
+                        SetBackColor();
+                    }
+                }
+            }
+        }
+        private void SetBackColor()
+        {
+            pnlMain.BackColor = Color.FromArgb(R, G, B);
+        }
+        private void txtG_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtG.Text)) {
+                if (int.TryParse(txtG.Text, out int g)) {
+                    if (g <= 255 && g >= 0) {
+                        tbG.Value = g;
+                        G = g;
+                        SetBackColor();
+                    }
+                }
+            }
+        }
+
+        private void txtB_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtB.Text)) {
+                if (int.TryParse(txtB.Text, out int b)) {
+                    if (b <= 255 && b >= 0) {
+                        B = b;
+                        tbB.Value = b;
+                        SetBackColor();
+                    }
+                }
+            }
         }
     }
 }
