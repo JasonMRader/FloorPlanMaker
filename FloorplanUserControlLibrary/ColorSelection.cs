75500168d529407e2a36cdfb7b0967ae9504b0d8
Jason rader <Jasonmrader@outlook.com>
2024-09-12 23:28:38 -0400

    Added next and previous comparisons

--- FloorplanUserControlLibrary/ColorSelection.cs
+++ FloorplanUserControlLibrary/ColorSelection.cs
@@ -17,6 +17,7 @@ namespace FloorplanUserControlLibrary
         private int R = 0;
         private int G = 0;
         private int B = 0;
+        private int num = 0;
         private ColorPair colorPair = new ColorPair();
         public ColorSelection()
         {
@@ -27,15 +28,7 @@ namespace FloorplanUserControlLibrary
         {
 
             for (int i = 1; i <= 15; i++) {
-                int spots = 2;
-                int previous = i - spots;
-                int next = i + spots;
-                if (previous < 1) {
-                    previous = SectionColorManager.SectionColors.Count - spots + i;
-                }
-                if (next > SectionColorManager.SectionColors.Count) {
-                    next = i - SectionColorManager.SectionColors.Count + spots;
-                }
+              
 
                 Button lbl = new Button() {
                     Text = i.ToString(),
@@ -49,16 +42,50 @@ namespace FloorplanUserControlLibrary
                     AllowDrop = true
                 };
 
+               
+                
                 lbl.Click += Label_Click;
-
+                //this.Controls.Add(lbl1Before);
+                //this.Controls.Add(lbl1After);
                 flowDefaults.Controls.Add(lbl);
 
             }
-
+           
 
 
         }
+        private void GetNeighboringSections()
+        {
+            int spots = 1;
+            int previous = num - spots;
+            int next = num + spots;
+            if (previous < 1) {
+                previous = 15 - spots + num;
+            }
+            if (next > 15) {
+                next = num - 15 + spots;
+            }
+            lbl1Before.Text = previous.ToString();
+            lbl1Before.Margin = new Padding(0);
+            //Size = new Size((flowLayoutPanel1.Width / 5), (flowLayoutPanel1.Height / 20)),
+            lbl1Before.TextAlign = ContentAlignment.MiddleCenter;
+            lbl1Before.Font = UITheme.LargeFont;
+            lbl1Before.BackColor = SectionColorManager.GetColorPair(previous).BackgroundColor;
+            lbl1Before.ForeColor = SectionColorManager.GetColorPair(previous).FontColor;
+            //lbl1Before.Dock = DockStyle.Left;
+            // lbl1Before.AllowDrop = true;
 
+
+            lbl1After.Text = next.ToString();
+            lbl1After.Margin = new Padding(0);
+            //Size = new Size((flowLayoutPanel1.Width / 5), (flowLayoutPanel1.Height / 20)),
+            lbl1After.TextAlign = ContentAlignment.MiddleCenter;
+            lbl1After.Font = UITheme.LargeFont;
+            lbl1After.BackColor = SectionColorManager.GetColorPair(next).BackgroundColor;
+            lbl1After.ForeColor = SectionColorManager.GetColorPair(next).FontColor;
+            //lbl1After.Dock = DockStyle.Right;
+            // lbl1After.AllowDrop = true;
+        }
         private void Label_Click(object? sender, EventArgs e)
         {
 
@@ -66,6 +93,7 @@ namespace FloorplanUserControlLibrary
 
         public void SetSectionColorPair(int sectionNumber)
         {
+            num = sectionNumber;
             this.colorPair = SectionColorManager.GetColorPair(sectionNumber);
             this.BackColor = colorPair.BackgroundColor;
             tbR.Value = colorPair.BackgroundColor.R;
@@ -81,6 +109,7 @@ namespace FloorplanUserControlLibrary
                 cbForeColor.Checked = false;
             }
             lblSectionNumber.Text = $"Section #{sectionNumber}";
+            GetNeighboringSections();
 
         }
 
