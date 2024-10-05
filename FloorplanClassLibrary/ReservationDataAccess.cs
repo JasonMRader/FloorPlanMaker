e6ad029323503b366f8c99f9b675b9783783beb5
Jason rader <Jasonmrader@outlook.com>
2024-10-05 14:27:46 -0400

    Added Reservation data API access

---
+++ FloorplanClassLibrary/ReservationDataAccess.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloorplanClassLibrary
+{
+    public static class ReservationDataAccess
+    {
+        private static string GetOpenTableClientID_UAT() {
+            string id = ConfigurationManager.AppSettings["OpenTableClientID_UAT"];
+            if (string.IsNullOrEmpty(id)) {
+                throw new InvalidOperationException("API key is missing.");
+            }
+            return id;
+        }
+        private static string GetOpenTableClientID()
+        {
+            string id = ConfigurationManager.AppSettings["OpenTableClientID"];
+            if (string.IsNullOrEmpty(id)) {
+                throw new InvalidOperationException("API key is missing.");
+            }
+            return id;
+        }
+        private static string GetOpenTableSecretID_UAT()
+        {
+            string id = ConfigurationManager.AppSettings["OpenTableSecretID_UAT"];
+            if (string.IsNullOrEmpty(id)) {
+                throw new InvalidOperationException("API key is missing.");
+            }
+            return id;
+        }
+        private static string GetOpenTableSecretID()
+        {
+            string id = ConfigurationManager.AppSettings["OpenTableSecretID"];
+            if (string.IsNullOrEmpty(id)) {
+                throw new InvalidOperationException("API key is missing.");
+            }
+            return id;
+        }
+    }
+}
