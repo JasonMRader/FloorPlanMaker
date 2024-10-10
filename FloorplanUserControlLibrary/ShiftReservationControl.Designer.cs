184b7ab9de394cd89792c91e2e9a2456dd265fa3
Jason rader <Jasonmrader@outlook.com>
2024-10-10 10:53:06 -0400

    Changes to reservation control

--- FloorplanUserControlLibrary/ShiftReservationControl.Designer.cs
+++ FloorplanUserControlLibrary/ShiftReservationControl.Designer.cs
@@ -40,10 +40,25 @@
             lbl3Largest = new Label();
             lbl4Largest = new Label();
             lbl5Largest = new Label();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            lbl1to4Count = new Label();
+            lbl5to8Count = new Label();
+            lbl9to14Count = new Label();
+            lbl15to19Count = new Label();
+            lbl20PlusCount = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -51,7 +66,7 @@
             panel1.BackColor = Color.FromArgb(180, 190, 200);
             panel1.Controls.Add(lblParties);
             panel1.Controls.Add(pictureBox1);
-            panel1.Location = new Point(7, 5);
+            panel1.Location = new Point(7, 10);
             panel1.Name = "panel1";
             panel1.Size = new Size(90, 37);
             panel1.TabIndex = 0;
@@ -82,7 +97,7 @@
             panel2.BackColor = Color.FromArgb(180, 190, 200);
             panel2.Controls.Add(lblCovers);
             panel2.Controls.Add(pictureBox2);
-            panel2.Location = new Point(105, 5);
+            panel2.Location = new Point(105, 10);
             panel2.Name = "panel2";
             panel2.Size = new Size(90, 37);
             panel2.TabIndex = 0;
@@ -123,70 +138,223 @@
             // 
             // label1
             // 
+            label1.BackColor = Color.FromArgb(175, 185, 195);
+            label1.Dock = DockStyle.Top;
             label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(0, 55);
+            label1.Location = new Point(0, 0);
             label1.Name = "label1";
-            label1.Size = new Size(200, 23);
+            label1.Size = new Size(90, 23);
             label1.TabIndex = 2;
-            label1.Text = "5 Largest Parties";
+            label1.Text = "5 Largest";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // lbl1Largest
             // 
             lbl1Largest.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl1Largest.Location = new Point(0, 78);
+            lbl1Largest.Location = new Point(0, 23);
             lbl1Largest.Name = "lbl1Largest";
-            lbl1Largest.Size = new Size(200, 23);
+            lbl1Largest.Size = new Size(90, 23);
             lbl1Largest.TabIndex = 2;
             lbl1Largest.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // lbl2Largest
             // 
             lbl2Largest.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl2Largest.Location = new Point(0, 101);
+            lbl2Largest.Location = new Point(0, 46);
             lbl2Largest.Name = "lbl2Largest";
-            lbl2Largest.Size = new Size(200, 23);
+            lbl2Largest.Size = new Size(90, 23);
             lbl2Largest.TabIndex = 2;
             lbl2Largest.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // lbl3Largest
             // 
             lbl3Largest.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl3Largest.Location = new Point(0, 124);
+            lbl3Largest.Location = new Point(0, 69);
             lbl3Largest.Name = "lbl3Largest";
-            lbl3Largest.Size = new Size(200, 23);
+            lbl3Largest.Size = new Size(90, 23);
             lbl3Largest.TabIndex = 2;
             lbl3Largest.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // lbl4Largest
             // 
             lbl4Largest.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl4Largest.Location = new Point(0, 147);
+            lbl4Largest.Location = new Point(0, 92);
             lbl4Largest.Name = "lbl4Largest";
-            lbl4Largest.Size = new Size(200, 23);
+            lbl4Largest.Size = new Size(90, 23);
             lbl4Largest.TabIndex = 2;
             lbl4Largest.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // lbl5Largest
             // 
             lbl5Largest.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl5Largest.Location = new Point(0, 170);
+            lbl5Largest.Location = new Point(0, 115);
             lbl5Largest.Name = "lbl5Largest";
-            lbl5Largest.Size = new Size(200, 23);
+            lbl5Largest.Size = new Size(90, 23);
             lbl5Largest.TabIndex = 2;
             lbl5Largest.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(180, 190, 200);
+            panel3.Controls.Add(label1);
+            panel3.Controls.Add(lbl5Largest);
+            panel3.Controls.Add(lbl1Largest);
+            panel3.Controls.Add(lbl4Largest);
+            panel3.Controls.Add(lbl2Largest);
+            panel3.Controls.Add(lbl3Largest);
+            panel3.Location = new Point(7, 55);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(90, 140);
+            panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(180, 190, 200);
+            panel4.Controls.Add(lbl20PlusCount);
+            panel4.Controls.Add(label7);
+            panel4.Controls.Add(lbl15to19Count);
+            panel4.Controls.Add(label6);
+            panel4.Controls.Add(lbl9to14Count);
+            panel4.Controls.Add(label5);
+            panel4.Controls.Add(lbl5to8Count);
+            panel4.Controls.Add(lbl1to4Count);
+            panel4.Controls.Add(label4);
+            panel4.Controls.Add(label3);
+            panel4.Controls.Add(label2);
+            panel4.Location = new Point(105, 55);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(90, 140);
+            panel4.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.FromArgb(175, 185, 195);
+            label2.Dock = DockStyle.Top;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(0, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 23);
+            label2.TabIndex = 3;
+            label2.Text = "By Size";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(0, 23);
+            label3.Margin = new Padding(0);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 23);
+            label3.TabIndex = 4;
+            label3.Text = "1-4";
+            label3.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(0, 46);
+            label4.Margin = new Padding(0);
+            label4.Name = "label4";
+            label4.Size = new Size(45, 23);
+            label4.TabIndex = 4;
+            label4.Text = "5-8";
+            label4.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(0, 69);
+            label5.Margin = new Padding(0);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 23);
+            label5.TabIndex = 4;
+            label5.Text = "9-14";
+            label5.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            label6.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(0, 92);
+            label6.Margin = new Padding(0);
+            label6.Name = "label6";
+            label6.Size = new Size(45, 23);
+            label6.TabIndex = 4;
+            label6.Text = "15-19";
+            label6.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            label7.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(0, 115);
+            label7.Margin = new Padding(0);
+            label7.Name = "label7";
+            label7.Size = new Size(45, 23);
+            label7.TabIndex = 4;
+            label7.Text = "20+";
+            label7.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // lbl1to4Count
+            // 
+            lbl1to4Count.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl1to4Count.Location = new Point(45, 23);
+            lbl1to4Count.Margin = new Padding(0);
+            lbl1to4Count.Name = "lbl1to4Count";
+            lbl1to4Count.Size = new Size(45, 23);
+            lbl1to4Count.TabIndex = 4;
+            lbl1to4Count.Text = "10";
+            lbl1to4Count.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lbl5to8Count
+            // 
+            lbl5to8Count.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl5to8Count.Location = new Point(45, 46);
+            lbl5to8Count.Margin = new Padding(0);
+            lbl5to8Count.Name = "lbl5to8Count";
+            lbl5to8Count.Size = new Size(45, 23);
+            lbl5to8Count.TabIndex = 4;
+            lbl5to8Count.Text = "10";
+            lbl5to8Count.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lbl9to14Count
+            // 
+            lbl9to14Count.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl9to14Count.Location = new Point(45, 69);
+            lbl9to14Count.Margin = new Padding(0);
+            lbl9to14Count.Name = "lbl9to14Count";
+            lbl9to14Count.Size = new Size(45, 23);
+            lbl9to14Count.TabIndex = 4;
+            lbl9to14Count.Text = "10";
+            lbl9to14Count.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lbl15to19Count
+            // 
+            lbl15to19Count.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl15to19Count.Location = new Point(45, 92);
+            lbl15to19Count.Margin = new Padding(0);
+            lbl15to19Count.Name = "lbl15to19Count";
+            lbl15to19Count.Size = new Size(45, 23);
+            lbl15to19Count.TabIndex = 4;
+            lbl15to19Count.Text = "10";
+            lbl15to19Count.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lbl20PlusCount
+            // 
+            lbl20PlusCount.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl20PlusCount.Location = new Point(45, 115);
+            lbl20PlusCount.Margin = new Padding(0);
+            lbl20PlusCount.Name = "lbl20PlusCount";
+            lbl20PlusCount.Size = new Size(45, 23);
+            lbl20PlusCount.TabIndex = 4;
+            lbl20PlusCount.Text = "10";
+            lbl20PlusCount.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // ShiftReservationControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoScroll = true;
-            Controls.Add(lbl5Largest);
-            Controls.Add(lbl4Largest);
-            Controls.Add(lbl3Largest);
-            Controls.Add(lbl2Largest);
-            Controls.Add(lbl1Largest);
-            Controls.Add(label1);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
             Controls.Add(flowLayoutPanel1);
             Controls.Add(panel2);
             Controls.Add(panel1);
@@ -197,6 +365,8 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -215,5 +385,18 @@
         private Label lbl3Largest;
         private Label lbl4Largest;
         private Label lbl5Largest;
+        private Panel panel3;
+        private Panel panel4;
+        private Label lbl20PlusCount;
+        private Label label7;
+        private Label lbl15to19Count;
+        private Label label6;
+        private Label lbl9to14Count;
+        private Label label5;
+        private Label lbl5to8Count;
+        private Label lbl1to4Count;
+        private Label label4;
+        private Label label3;
+        private Label label2;
     }
 }
