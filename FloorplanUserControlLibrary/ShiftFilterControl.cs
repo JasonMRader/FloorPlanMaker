67e1b13f60fc4b52ae4bc6025f46b9e298c2df4c
Jason rader <Jasonmrader@outlook.com>
2024-11-01 23:55:47 -0400

    removed unnecissary using from ShiftFilterControl

--- FloorplanUserControlLibrary/ShiftFilterControl.cs
+++ FloorplanUserControlLibrary/ShiftFilterControl.cs
@@ -9,7 +9,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace FloorplanUserControlLibrary
 {
